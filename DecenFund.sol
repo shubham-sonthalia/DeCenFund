bec6c87b4b6ecbc0ad2e56b8ff4c2b09017af1fb
Shubham <shubham18366@iiitd.ac.in>
2021-10-25 00:21:21 +0530

    Making multiple contracts to interact with each other

---
+++ DecenFund.sol
@@ -0,0 +1,80 @@
+pragma solidity >=0.7.0 < 0.9.0;
+
+import "./Project.sol";
+
+// SPDX-License-Identifier:UNLICENSED
+contract DecenFund{
+    address public owner;
+    uint public numOfProjects;
+
+    mapping (uint => Project) public projects;
+
+    event EventProjectCreated(uint id, string title, Project addr, address creator);
+    event DonationSent(address projectAddress, address beneficiary, uint amount);
+
+    event Error(string message);
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You don't have the rights to perform this action.");
+        _;
+    }
+    
+    constructor() {
+        owner = msg.sender;
+        numOfProjects = 0;
+    }
+ 
+
+    function createProject(uint _targetAmount, uint _targetInDays, string calldata _title) external returns (Project projectAddress) {
+
+        if (_targetAmount <= 0) {
+            emit Error("Project funding goal must be greater than 0");
+            revert("Project funding goal must be greater than 0");
+        }
+
+        if (_targetInDays <= 0) {
+            emit Error("Project deadline must be greater than the current block");
+            revert("Project deadline must be greater than the current block");
+        }
+
+        Project p = new Project(_targetAmount, _targetInDays, _title, msg.sender);
+        projects[numOfProjects] = p;
+        emit EventProjectCreated(numOfProjects, _title, p, msg.sender);
+        numOfProjects++;
+        return p;
+    }
+
+   
+    function donate(address _projectAddress) external payable returns (bool successful) { 
+        
+        if(msg.value <= 0){
+            emit Error("Donations must be greater than 0 wei");
+            revert("Donations must be greater than 0 wei");
+        }
+        Project deployedProject = Project(_projectAddress);
+        // Check that there is actually a Project contract at that address
+        if (deployedProject.DecenFund() == address(0)) {
+            emit Error("Project contract not found at address");
+            revert("Project contract not found at address");
+        }
+
+        // Check that fund call was successful
+        if (deployedProject.fund(msg.sender)) {
+            emit DonationSent(_projectAddress, msg.sender, msg.value);
+            return true;
+        } else {
+            emit Error("Contribution did not send successfully");
+            return false;
+        }
+    }
+
+    function kill() public onlyOwner {
+    address payable Owner = payable(owner);
+        selfdestruct(Owner);
+    }
+    fallback() external{
+        revert('Fallback error');
+    }
+}
+
+
---
+++ Project.sol
@@ -0,0 +1,156 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+// SPDX-License-Identifier:UNLICENSED
+// To do: 
+
+// 1. Project should be able to receive payments. 
+// 2. User Contract. And donors and beneficiaries could inherit from it. 
+// 3. Need to make a lot of events to allow communication between frontend and backend.
+// 4. 
+contract Project 
+{
+    // A data type to store the state of the project. 
+    enum STATE {INPROGRESS, FINISHED}
+    // A struct that stores all the details unique to the project. 
+    struct Properties
+    {
+        uint targetAmount;
+        uint deadline;
+        address creator;
+        string title;
+        uint startTime;
+    }
+    // A struct that holds vital information of a donation. 
+     struct Donation 
+     {
+        uint amount;
+        address donor;
+     }
+     // Some state variables.
+     uint public collectedAmount;
+     uint public donorsCount;
+     STATE public state;
+     Properties public properties;
+     address public DecenFund; 
+     
+     
+     mapping (address => uint) donors;
+     mapping (uint => Properties) projects;
+     
+    event EventDonationReceived(address projectAddress, address contributor, uint amount);
+    event EventPayoutInitiated(address projectAddress, address owner, uint totalPayout);
+    event EventTargetedAmountReached(address projectAddress, uint totalFunding);
+    event EventFundingFailed(address projectAddress, uint totalFunding);
+    
+    event Error(string message);
+    
+    modifier onlyDecenFund {
+        if (DecenFund != msg.sender)
+        {
+            emit Error("Unauthrorized access!!");
+            revert("Unauthrorized access!!");
+        }
+        _;
+    }
+     constructor (uint _targetAmount, uint _targetInDays, string memory _title, address _creator) {
+
+        uint deadline = block.timestamp + _targetInDays*86400;
+        DecenFund = msg.sender;
+        properties = Properties({
+            targetAmount: _targetAmount,
+            deadline: deadline,
+            title: _title,
+            creator: _creator,
+            startTime:block.timestamp
+        });
+        collectedAmount = 0;
+        donorsCount = 0;
+        state = STATE.INPROGRESS;
+        // After receiving all this information, my project is created. 
+    }
+    function getProject() public view returns (string memory, uint, uint, address, uint, uint, address, address) {
+        return (properties.title,
+                properties.targetAmount,
+                properties.deadline,
+                properties.creator,
+                collectedAmount,
+                donorsCount,
+                DecenFund,
+                address(this));
+    }
+    
+    function fund(address _donator) payable onlyDecenFund external returns (bool successful) 
+    {
+        address payable _Donator = payable(_donator);
+  
+        if (block.timestamp > properties.deadline) 
+        {
+            emit EventFundingFailed(address(this), collectedAmount);
+            
+            if (!_Donator.send(msg.value)) 
+            {
+                emit Error("Project deadline has passed, problem returning contribution");
+                revert("Project deadline has passed, problem returning contribution");
+            } 
+            return false;
+        }
+
+        // 2. Check that funding goal has not already been met
+        if (collectedAmount >= properties.targetAmount) 
+        {
+            emit EventTargetedAmountReached(address(this), collectedAmount);
+            if (!_Donator.send(msg.value)) 
+            {
+                emit Error("Project deadline has passed, problem returning contribution");
+                revert("Project deadline has passed, problem returning contribution");
+            }
+            payout();
+            return false;
+        }
+
+        // determine if this is a new contributor
+        uint prevContributionBalance = donors[_donator];
+
+        // Update contributor's balance
+        donors[_donator] += msg.value;
+
+        collectedAmount += msg.value;
+
+
+        // Check if contributor is new and if so increase count
+        if (prevContributionBalance == 0) {
+            donorsCount++;
+        }
+
+       emit  EventDonationReceived(address(this), _donator, msg.value);
+
+        // Check again to see whether the last contribution met the fundingGoal 
+        if (collectedAmount >= properties.targetAmount) {
+            emit EventTargetedAmountReached(address(this), collectedAmount);
+            payout();
+        }
+
+        return true;
+    }
+    function payout() private returns (bool successful) 
+    {
+        uint amount = collectedAmount;
+
+        // prevent re-entrancy
+        collectedAmount = 0;
+        address payable Creator = payable(properties.creator);
+        if (Creator.send(amount)) 
+        {
+            return true;
+        } 
+        else 
+        {
+            collectedAmount = amount;
+            return false;
+        }
+    }
+    fallback() external{
+        revert('Fallback error');
+    }
+    
+}
