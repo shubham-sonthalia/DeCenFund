f354ac45c9a1d1a3c71a6d65345f318d18f02c9d
Shubham Sonthalia <54079322+shubham-sonthalia@users.noreply.github.com>
2021-12-14 14:29:39 +0530

    Final changes to the Project

--- Project.sol
+++ Project.sol
@@ -2,17 +2,9 @@ pragma solidity >=0.7.0 <0.9.0;
 import './DecenFund.sol';
 
 // SPDX-License-Identifier:UNLICENSED
-// To do: 
 
-// 1. Project should be able to receive payments. 
-// 2. User Contract. And donors and beneficiaries could inherit from it. 
-// 3. Need to make a lot of events to allow communication between frontend and backend.
-// 4. 
 contract Project 
 {
-    // A data type to store the state of the project. 
-    enum STATE {INPROGRESS, FINISHED}
-    // A struct that stores all the details unique to the project. 
     struct Properties
     {
         uint targetAmount;
@@ -21,21 +13,13 @@ contract Project
         uint startTime;
         string emailIDOfCreator;
     }
-    // A struct that holds vital information of a donation. 
-     struct Donation 
-     {
-        uint amount;
-        address donor;
-     }
+     
      // Some state variables.
      uint public collectedAmount;
-     uint public donorsCount;
-     STATE public state;
      Properties public properties;
      address public DF; 
      address payable public creator;
      
-     mapping (address => uint) public donors;
      mapping (uint => Properties) projects;
      
     event EventDonationReceived(address projectAddress, address contributor, uint amount);
@@ -53,11 +37,9 @@ contract Project
         }
         _;
     }
-    
      constructor (uint _targetAmount, uint _targetInDays, string memory _title, address payable _creator, string memory _email) {
 
-        // uint deadline = block.timestamp + _targetInDays*86400;
-        uint deadline = block.timestamp + _targetInDays*60;
+        uint deadline = block.timestamp + _targetInDays*86400;
         DF = msg.sender;
         properties = Properties({
             targetAmount: _targetAmount,
@@ -68,19 +50,8 @@ contract Project
         });
         creator = _creator;
         collectedAmount = 0;
-        donorsCount = 0;
-        state = STATE.INPROGRESS;
-        // After receiving all this information, my project is created. 
     }
-    function getProject() public view returns (string memory, uint, uint, uint, uint, address, address) {
-        return (properties.title,
-                properties.targetAmount,
-                properties.deadline,
-                collectedAmount,
-                donorsCount,
-                DF,
-                address(this));
-    }
+
     function fund(address _donator, uint _fval) payable onlyDecenFund external returns (bool successful) 
     {
         address payable _Donator = payable(_donator);
@@ -101,30 +72,12 @@ contract Project
             payout();
             return false;
         }
-
-        // determine if this is a new contributor
-        uint prevContributionBalance = donors[_donator];
-
-        // Update contributor's balance
-        donors[_donator] += _fval;
-
         collectedAmount += _fval;
-
-
-        // Check if contributor is new and if so increase count
-        if (prevContributionBalance == 0) {
-            donorsCount++;
-        }
-
        emit  EventDonationReceived(address(this), _donator, _fval);
 
         // Check again to see whether the last contribution met the fundingGoal 
         if (collectedAmount >= properties.targetAmount) {
             emit EventTargetedAmountReached(address(this), collectedAmount);
-            // if(!payout()){
-            //     emit Error("Payout not called");
-            //     revert("Payout not called");
-            // }
             payout();
         }
         return true;
